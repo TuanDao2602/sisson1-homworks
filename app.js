82299b4ce5ec5f0c1f08a6df2fcf29b3d56b9cb4
TuanDao2602 <110024900+TuanDao2602@users.noreply.github.com>
2022-07-28 20:34:46 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+// Bài 1
+// Viết chương trình nhập điểm của một sinh viên cho các môn: Vật lý, Hóa học,
+// và Sinh học. Sau đó hiển thị điểm trung bình và tổng của những điểm này.
+// let A = prompt("nhập tên sinh viên");
+// let B = prompt("nhập mã sinh viên")
+// let C = prompt("ngày tháng năm= sinh");
+// alert(`tên sinh viên là : ${A}
+//       MÃ SINH VIÊN LÀ : ${B} 
+//       NGÀY THÁNG NĂM SINH LÀ ${C}`);
+// let Diemly = Number(prompt("nhập điểm vật lý"));
+// let Diemhoa = Number(prompt("nhập điểm hóa học"));
+// let Diemsinh = Number(prompt("nhập điểm sinh học"));
+// let Tong = (Diemly + Diemhoa + Diemsinh );
+// let diemTB = (Tong)/3;
+// alert("điểm tổng là :"+ Tong);
+// alert("điểm trung bình của bạn là :" + diemTB)
+
+
+//Bài 2 : Viết chương trình nhập một giá trị là độ 0C (Celsius) và chuyển nó sang độ 0F
+// (Fahrenheit). [Hướng dẫn: C/5 = (F-32)/9]
+
+
+// let doC = Number(prompt("nhập giá trị của độ C"));
+// let doF = doC*1.8+32;
+// alert("Nhiệt độ F của bạn là :" + doF);
+
+
+//Bài 3 + 4  Viết chương trình tính chu vi và diện tích hình tròn
+// let bankinh = Number(prompt("nhập vào bán kính hình tròn"));
+// let chuvi = bankinh*2*3.14 ;
+// let dientich = bankinh**2*3.14;
+// alert("chuvi của hình tròn là :"+ chuvi);
+// alert("Diện tích hình tròn của bạn là:" + dientich);
+
